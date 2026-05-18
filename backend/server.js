00e46920058c48c2298f9dbc483e790cc2432e6e
abdeljaoued <125016172+dalios009@users.noreply.github.com>
2026-05-18 08:38:07 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,8 @@ const logger = require('./utils/logger');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// Trust Railway's proxy
 app.set('trust proxy', 1);
+
 
 app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
 
